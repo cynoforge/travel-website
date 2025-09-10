693d1bfc8bec4c745e8c183a4f77ac4b963a8ed3
Nemash Randeniya <nemashrandeniya2001@gmail.com>
2025-09-10 22:25:48 +0530

    changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-  theme: {},
+  theme: {
+    extend: {
+      fontFamily: {
+        // sans: ['Ubuntu', 'ui-sans-serif', 'system-ui'],
+        display: ['Roboto', 'ui-sans-serif', 'system-ui']
+      }
+    }
+  },
   plugins: [],
 };
